13503562ca5a4e946c9681352ff886902aa2dbc9
a <antoniomusciano@gmail.com>
2023-11-25 15:41:30 -0500

    refactor bc idk wtf im doing
    im ass

--- bot.py
+++ bot.py
@@ -1,37 +1,33 @@
 import discord
 import responses
 import keys1
+import random
 from discord.ext import commands, tasks
 
-# Create default intents
-intents = discord.Intents.all()
-
 # Create a bot instance with specified command prefix and intents
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='-', intents=discord.Intents.all())
 
 
-async def sendMessage(message, user, private):
-    try:
-        response = responses.responseHandler(user)
-        await message.author.send(response) if private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+@bot.event
+async def on_ready():
+    print(f'Success: The bot is now on.')
 
 
-def run():
-    @bot.event
-    async def on():
-        print(f'The bot is now on.')
+'''commands'''
 
-    @bot.event
-    async def on_message(message):
-        # Ignore messages from the bot itself
-        if message.author == bot.user:
-            return
 
-        username = str(message.author)
-        message = str(message.content)
-        print(f"{message} was from {username} ")
-        # Will only retrieve message if Pinged
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
 
-    bot.run(keys1.DISCORD_TOKEN)
+
+@bot.command()
+async def hort(ctx):
+    random_number = random.randint(0, 10)
+    if random_number > 5:
+        await ctx.send('HEADS')
+    else:
+        await ctx.send('TAILS')
+
+
+bot.run(keys1.DISCORD_TOKEN)
--- responses.py
+++
@@ -1,9 +0,0 @@
-def responseHandler(message) -> str:
-    process = message.lower()
-
-
-    if process == 'hello':
-        return 'Yeo'
-
-    if process == 'ping':
-        return 'pong!'
