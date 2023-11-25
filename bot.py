48721f8d317cbdb1ad1c8e676856d43f7e4c9b42
a <antoniomusciano@gmail.com>
2023-11-25 15:54:55 -0500

    bot status cycle

--- bot.py
+++ bot.py
@@ -3,14 +3,22 @@ import responses
 import keys1
 import random
 from discord.ext import commands, tasks
+from itertools import cycle
 
 # Create a bot instance with specified command prefix and intents
 bot = commands.Bot(command_prefix='-', intents=discord.Intents.all())
 
+bot_status = cycle(['HIGH TEMPO DUELS', 'JOIN HTD NOW', 'HTD SEASON 3','hort...I SAID HORT'])
+
+
+@tasks.loop(seconds=45)
+async def change_status():
+    await bot.change_presence(activity=discord.Game(next(bot_status)))
 
 @bot.event
 async def on_ready():
     print(f'Success: The bot is now on.')
+    change_status.start()
 
 
 '''
@@ -21,7 +29,9 @@ this is a way to have multiple names for your command
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('Pong!')
+    bot_latency = round(bot.latency * 1000)
+
+    await ctx.send(f'Pong!  \nResponse time {bot_latency} ms.')
 
 
 @bot.command()
