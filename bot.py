1426bfeb330010aa3c508a0fcc9c86b9a2365181
a <antoniomusciano@gmail.com>
2023-11-26 00:43:23 -0500

    google sheets api and stuff omg
    yay

--- bot.py
+++ bot.py
@@ -1,13 +1,29 @@
 import discord
 import keys1
 import random
+import gspread
 from discord.ext import commands, tasks
 from itertools import cycle
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 # Create a bot instance with specified command prefix and intents
 bot = commands.Bot(command_prefix='-', intents=discord.Intents.all())
 
 bot_status = cycle(['HIGH TEMPO DUELS', 'JOIN HTD NOW', 'HTD SEASON 3', 'hort...I SAID HORT'])
+
+
+scopes = ['https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/drive']
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    'D:\\coding\\WinstonBot\\gkey.json', scopes=scopes)
+
+file = gspread.authorize(creds)
+workbook = file.open("HTD3")
+sheet = workbook.sheet1
+
+
 
 
 @tasks.loop(seconds=45)
@@ -50,14 +66,17 @@ async def wyr(ctx):
         await ctx.send(lines[random.randint(1, 30)])
 
 
-
-
 @bot.command()
 async def eightball(ctx):
     # Open the file and read all lines into a list
     with open("8.txt") as open_file:
         lines = open_file.readlines()
         await ctx.send(lines[random.randint(1, 30)])
+
+@bot.command()
+async def players(ctx):
+    for cell in sheet.range('B4:B11'):
+        await ctx.send(cell.value)
 
 
 bot.run(keys1.DISCORD_TOKEN)
