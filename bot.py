8274745e2c93f014d7b8ead616af6072affb3fc2
a <antoniomusciano@gmail.com>
2023-11-27 21:31:50 -0500

    sexy style

--- bot.py
+++ bot.py
@@ -40,36 +40,56 @@ this is a way to have multiple names for your command
 
 @bot.command()
 async def ping(ctx):
-    # Takes -ping as an input and spits out the latency of the output
-    bot_latency = round(bot.latency * 1000)
+    try:
 
-    await ctx.send(f'Pong!  \nResponse time {bot_latency} ms.')
+        # Takes -ping as an input and spits out the latency of the output
+        bot_latency = round(bot.latency * 1000)
+
+        await ctx.send(f'Pong!  \nResponse time {bot_latency} ms.')\
+
+    except Exception as e:
+        print(f"Error: {e}")
+        await ctx.send("STOP PINGING PLEASE.")
+
 
 
 @bot.command()
 async def hort(ctx):
-    # Takes -hort as an input and then spits out HEADS or TAILS
-    result = 'HEADS' if random.randint(0, 10) > 5 else 'TAILS'
-    await ctx.send(result)
+    try:
+
+        # Takes -hort as an input and then spits out HEADS or TAILS
+        result = 'HEADS' if random.randint(0, 10) > 5 else 'TAILS'
+        await ctx.send(result)
+    except Exception as e:
+        print(f"Error: {e}")
+        await ctx.send("? what do you want lol")
 
 
 @bot.command()
 async def wyr(ctx):
-    # Open the file and read all lines into a list
-    with open("wyr.txt") as open_file:
-        lines = open_file.readlines()
-        length = len(lines)
-        # print(length)
-        await ctx.send(lines[random.randint(1, length)])
+    try:
+        # Open the file and read all lines into a list
+        with open("wyr.txt") as open_file:
+            lines = open_file.readlines()
+            length = len(lines)
+            # print(length)
+            await ctx.send(lines[random.randint(1, length - 1)])
+    except Exception as e:
+        print(f"Error: {e}")
+        await ctx.send("Relax Pal.")
 
 
-@bot.command(aliases=["8ball","8"])
+@bot.command(aliases=["8ball", "8"])
 async def eightball(ctx):
-    # Open the file and read all lines into a list
-    with open("8.txt") as open_file:
-        lines = open_file.readlines()
-        length = len(lines)
-        await ctx.send(lines[random.randint(1, length)])
+    try:
+        # Open the file and read all lines into a list
+        with open("8.txt") as open_file:
+            lines = open_file.readlines()
+            length = len(lines)
+            await ctx.send(lines[random.randint(1, length)])
+    except Exception as e:
+        print(f"Error: {e}")
+        await ctx.send("Relax Pal.")
 
 
 @bot.command()
@@ -99,7 +119,7 @@ async def players_d2(ctx):
     try:
         sheet_index = 1  # Index of the sheet
         sheet = workbook.get_worksheet(sheet_index)
-        values = sheet.range('A4:A11')
+        values = sheet.range('A4:A17')
 
         # Creating an embed
         embed = discord.Embed(title="Players in DIVISION II", color=discord.Color.blue())
